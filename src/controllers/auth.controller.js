fd44689608e6accf66b95bed967a3ac5056dcbef
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-14 13:21:12 -0300

    feat: Create sign up method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,47 @@
-export const signup = (req, res) => {
-  res.send("Signup route");
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error("Error in signup controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 export const login = (req, res) => {
   res.send("Login route");
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
