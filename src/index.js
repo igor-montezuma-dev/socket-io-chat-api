6b28d44c4122ec54510144a5f09e40706554d26e
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-14 12:31:36 -0300

    config: Add mongo db configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
+import { connectDb } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
+const PORT = process.env.PORT;
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  connectDb();
 });
