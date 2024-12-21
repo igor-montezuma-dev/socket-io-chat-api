c47b3b325907734c878f90c31879cd7ae4e30e0f
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-21 00:33:17 -0300

    fix: Fix cloudinary config

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -5,9 +5,9 @@ import { config } from "dotenv";
 config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDiNARY_CLOUD_NAM,
-  api_key: process.env.CLOUDiNARY_API_KEY,
-  api_secret: process.env.CLOUDiNARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
